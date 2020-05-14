4f9032b46cedbac2b481acee180854b53b1d49f3
Blaine Heffron <bheffron@vols.utk.edu>
2020-05-14 12:18:30 -0400

    adding ability to specify min and max en

--- LaunchXSecMCNP.py
+++ LaunchXSecMCNP.py
@@ -25,14 +25,20 @@ def main():
     parser.add_argument("--Z",     help="element Z value")
     parser.add_argument("--mat", help="material number")
     parser.add_argument("--n",     help="number of events for each energy")
+    parser.add_argument("--minen",     help="minimum energy to simulate")
+    parser.add_argument("--maxen",     help="maximum energy to simulate")
     options = parser.parse_args()
     if options.kill:
         os.system("killall -9 parallel")
         os.system("killall -9 mcnp6")
     tempdir = "templates"
     datdir =  join(expanduser(os.environ["MAGEDIR"]),"validation/NeutronInteractions/dat")
+    minen = float(options.minen) if options.minen else 0.0
+    maxen = float(options.maxen) if options.maxen else 1.0E10
     energies = readFile(join(datdir,"neutronEnergies.txt"))
     energies = [e.rstrip() for e in energies]
+    energies = [e for e in energies if float(e) <= maxen and float(e) >= minen]
+    print("energies is ",energies)
     template = join(tempdir,"xsec_template.c")
     data = {}
     for line in ELEDAT.split("\n"):
@@ -42,8 +48,10 @@ def main():
             data[dat[1] + "000"] = dat[2].rstrip()
             if(len(dat[0]) == 1):
                 data[dat[1] + "00" + dat[0]] = dat[2].rstrip()
-            else:
+            elif(len(dat[0]) == 2):
                 data[dat[1] + "0" + dat[0]] = dat[2].rstrip()
+            else:
+                data[dat[1] + dat[0]] = dat[2].rstrip()
     extra = {}
     if(options.Z):
         extra["matnum"] = options.Z + "000"
@@ -52,8 +60,8 @@ def main():
         extra["matnum"] = options.mat
         extra["density"] = data[options.mat]
     numevts = options.n
-    L = JobLauncher(join("XSec",extra["matnum"]),template,numevts,0,extra)
-    L.launch_en_sims(energies)
+    #L = JobLauncher(join("XSec",extra["matnum"]),template,numevts,0,extra)
+    #L.launch_en_sims(energies)
 
 if __name__=="__main__":
     main()
